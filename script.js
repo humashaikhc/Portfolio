8488ac23d98dfd33f64272e33c56a48a91027626
humashaikhc <fatma_shaikh21@yahoo.com>
2023-02-22 13:29:18 +0000

    smooth scroll in js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let bars = document.querySelector("#menu");
 let header = document.querySelector(".header");
+let links = document.querySelector(".navbar");
 
 bars.addEventListener("click", () => {
   bars.classList.toggle("fa-times");
@@ -10,4 +11,11 @@ window.addEventListener("onscroll", () => {
   bars.classList.remove("fa-times");
   header.classList.toggle("active");
 });
+
+links.addEventListener("click", ()=> {
+  if (window.innerWidth <= 768 && bars){
+    header.classList.remove('active')
+    links.classList.toggle('active');
+  }
+})
 
